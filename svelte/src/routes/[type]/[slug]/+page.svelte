b86e3438392bf0b912ea07b967dff8c5ce2ed9fe
Justin Lopato <justin@lopato.org>
2025-08-09 12:15:28 -0500

    fix date width

--- svelte/src/routes/[type]/[slug]/+page.svelte
+++ svelte/src/routes/[type]/[slug]/+page.svelte
@@ -12,8 +12,8 @@
 </svelte:head>
 
 <div class="flex flex-row items-center">
-  <h1 class="text-2xl flex-grow font-medium tracking-tighter">{name}</h1>
-  <p>{dateFrom(date)}</p>
+  <h1 class="text-xl sm:text-2xl flex-grow font-medium tracking-tighter">{name}</h1>
+  <p class="min-w-fit">{dateFrom(date)}</p>
 </div>
 <div class="py-8 h-30 flex flex-col gap-2">
   <img src={thumbnail.url} alt={thumbnail.caption} class="w-full h-48 object-cover rounded-lg" />

996b59c35e69b42bcf9b53af9c5e9de18c7fa909
Justin Lopato <justin@lopato.org>
2026-01-11 15:10:50 -0600

    moved env file

--- svelte/svelte.config.js
+++ svelte/svelte.config.js
@@ -1,19 +1,16 @@
-import adapter from '@sveltejs/adapter-vercel';
-import { installPolyfills } from '@sveltejs/kit/node/polyfills';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-vercel";
+import { installPolyfills } from "@sveltejs/kit/node/polyfills";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	vitePlugin: installPolyfills(),
-	kit: {
-		adapter: adapter(),
-		env: {
-			dir: '../'
-		}
-	}
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  vitePlugin: installPolyfills(),
+  kit: {
+    adapter: adapter(),
+  },
 };
 
 export default config;
